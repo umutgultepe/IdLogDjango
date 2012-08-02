c5a0bb7f4e3f7e44ecd418dd5169edfa50f557f4
Alp Sayin <alpsayin@gmail.com>
2012-08-02 03:31:43 +0300

    fixed some bugs and added docs

--- IdLogDjango/logs/LinkScanner.py
+++ IdLogDjango/logs/LinkScanner.py
@@ -1,6 +1,15 @@
 #!/bin/usr/env python
 
 def findLink(content):
+	"""
+	findLink(content):
+		parms:
+			content: a string to be scanned
+		returns:
+			start_end: a list with two indexes indicating
+			the start and end indexes of the first found link.
+			If no link is found then, an empty list is returned	
+	"""
 	www = 'www.'
 	windex = 0 
 	http = 'http'
@@ -15,7 +24,7 @@ def findLink(content):
 					if(content[j].isspace()):
 						start_end.append(j)
 						return start_end
-				start_end.append(j)
+				start_end.append(j+1)
 				return start_end
 		else:
 			windex=0
@@ -27,26 +36,65 @@ def findLink(content):
 					if(content[j].isspace()):
 						start_end.append(j)
 						return start_end
-				start_end.append(j)
+				start_end.append(j+1)
 				return start_end
 		else:
 			httpindex=0
 	return start_end
 
 def stripLink(content):
+	"""
+	stripLink(content):
+		parms:
+			content: string to be stripped into links and non-links
+		returns:
+			striplist:	If no link is found then the content itself is returned in a list.
+			If a link is found in the string and it's the end of the string
+			then a list with two elements is returned, first one being the non-links
+			second one being the link.
+			If a link is found in the string and it's NOT the end of the string
+			then a list with three elements is returned, first one being the non-links
+			second one being the link and the third one being the rest of the string.
+	"""
+	striplist = []
 	start_end = findLink(content)
+	# print start_end
 	if(len(start_end) == 0):
-		return [content]
+		striplist.append(content)
+		return striplist
 	else: 
-		if(start_end[1] == len(content)-1):
-			return [ content[:start_end[0]-1], content[start_end[0]:] ]
+		if(start_end[0] == 0):
+			striplist.append('')
+		if(start_end[1] == len(content)):
+			striplist.append(content[:start_end[0]])
+			striplist.append(content[start_end[0]:start_end[1]])
 		else:
-			return [ content[:start_end[0]-1], content[start_end[0]:start_end[1]], content[start_end[1]:] ]
+			if(start_end[0] != 0):
+				striplist.append(content[:start_end[0]])
+			striplist.append(content[start_end[0]:start_end[1]])
+			striplist.append(content[start_end[1]:])
+
+	return striplist
 
 def scanLinks(content):
+	"""
+	scanLinks(content):
+		parms:
+			content: string to be analyzed and stripped into links and non-links
+		returns:
+			A list containg linksList and isLink lists
+				linksList:
+					An ordered string list of stripped strings of the content. If the
+					strings in this list are concataneted in the natural order, the content
+					can be built without modification
+				isLink:
+					An ordered boolean list indicating whether the string in i'th index of
+					the linksList is a link or not
+	"""
 	linksList = []
 	isLink = []
 	tempList = stripLink(content)
+	# print "templist = " + str(tempList)	
 	while(len(tempList) != 1):
 		if(len(tempList) == 2):
 			linksList.extend(tempList)
@@ -61,28 +109,17 @@ def scanLinks(content):
 				isLink.append(False)
 				isLink.append(True)
 				tempList = stripLink(tempList[2])
+				# print "templist = " + str(tempList)
+
+	linksList.extend(tempList)
+	isLink.append(False)
 
 	# print linksList
 	# print isLink
 	return [linksList, isLink]
 
 if __name__ == '__main__':
-	content = """
-	word0
-		www.alpsayin.com word1 word2 word3
-		http://www.alpsayin.com word4 word5
-		word6
-		www.alpsayin.com
-"""
-	index = findLink(content)
-	print index
-	print content[index[0]:]
-	stripped = stripLink(content)
-	print stripped
-	stripped = stripLink(stripped[2])
-	print stripped
-	stripped = stripLink(stripped[2])
-	print stripped
-	print ''
+	content = """www.0.com text http://www.1.com text1 text2"""
+
 	scanresults = scanLinks(content)
 	print str(scanresults[0])+'\n'+str(scanresults[1])
