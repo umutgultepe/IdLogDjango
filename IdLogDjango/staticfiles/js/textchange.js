b9d75c3bb3bbe7ae3df1990616ff62021ff72983
Sarp Ulas <sarpulas@gmail.com>
2012-08-02 18:08:48 +0300

    textChange
    Text délétion bug fixed by a new method, base, index and new entry
    references to it are made

---
+++ IdLogDjango/staticfiles/js/textchange.js
@@ -0,0 +1,12 @@
+function textChange(txt)
+	{
+		var result
+		if (txt = "-Quick Entry.") 
+			result = ''
+		else
+			if (txt = "-Entry content goes here.")
+				result = ''
+			else
+				result = txt
+		return result
+	}
