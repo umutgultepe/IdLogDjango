3c4d9c0c2284127a79a17c20521bcc2d8942e692
Alp Sayin <alpsayin@gmail.com>
2012-08-03 04:23:18 +1200

    changed the url conf and settings to include ssl middleware

--- IdLogDjango/IdLogDjango/settings.py
+++ IdLogDjango/IdLogDjango/settings.py
@@ -110,13 +110,14 @@ TEMPLATE_LOADERS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'middleware.SSLRedirect.SSLRedirect',
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'IdLogDjango.urls'
--- IdLogDjango/IdLogDjango/urls.py
+++ IdLogDjango/IdLogDjango/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+ForceSSL = False
+
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^logs/', include('logs.urls')),
-    url(r'^auth/', include('authenticate.urls')),
-    url(r'^$', "authenticate.views.login_page"),
+    url(r'^admin/', include(admin.site.urls), {'SSL':ForceSSL}),
+    url(r'^logs/', include('logs.urls'), {'SSL':ForceSSL}),
+    url(r'^auth/', include('authenticate.urls'), {'SSL':ForceSSL}),
+    url(r'^$', "authenticate.views.login_page", {'SSL':ForceSSL}),
 )
 
