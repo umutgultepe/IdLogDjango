a2cc3ad12ed42afdcccb1ac81a0c3f9949b24122
Sarp Ulas <sarpulas@gmail.com>
2012-08-03 12:26:46 +0300

    text deletion bug fixed
    Scripts moved to individual index/entry files as the global variable is
    loaded once otherwise
    
    textchange.js is to be deprecated

--- IdLogDjango/staticfiles/js/textchange.js
+++ IdLogDjango/staticfiles/js/textchange.js
@@ -2,11 +2,19 @@ function textChange(txt)
 	{
 		var result
 		if (txt = "-Quick Entry.") 
+		{
 			result = ''
+		}
 		else
+		{
 			if (txt = "-Entry content goes here.")
+			{
 				result = ''
+			}
 			else
+			{
 				result = txt
+			}
+		}
 		return result
 	}
