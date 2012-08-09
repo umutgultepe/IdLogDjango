59ede62ccdbc956eefa243a7eb0708bf519a0558
Alp Sayin <alpsayin@gmail.com>
2012-08-09 17:50:17 +0300

    added root user as comment for easy switching

--- IdLogDjango/IdLogDjango/settings.py
+++ IdLogDjango/IdLogDjango/settings.py
@@ -20,6 +20,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'idlogdjango_test',                      # Or path to database file if using sqlite3.
         'USER': 'idlogdjango_test',                      # Not used with sqlite3.
+        #'USER': 'root',                      # Not used with sqlite3.
         'PASSWORD': '123456',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
@@ -27,16 +28,6 @@ DATABASES = {
  }
 #===============================================================================
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#        'NAME': 'IdLogTest',                      # Or path to database file if using sqlite3.
-#        'USER': 'g_django',                      # Not used with sqlite3.
-#        'PASSWORD': '123456',                  # Not used with sqlite3.
-#        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#    }
-#}
 
 
 # Local time zone for this installation. Choices can be found here:
